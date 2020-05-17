5d830d5a226d13deb312478ca1475a4261ff5863
mounteverestconsulting417 <65508518+mounteverestconsulting417@users.noreply.github.com>
2020-05-17 15:28:49 -0400

    Create HelloWorldController.java

---
+++ java_web_app/src/main/controller/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.demo.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	String message = "Welcome to Spring MVC, ";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+
+		String environment = System.getenv("ENVIRONMENT");
+
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("environment", environment);
+    if (name == "") {
+			mv.addObject("name", "World");
+		} else {
+			mv.addObject("name", name);
+		}
+		return mv;
+	}
+}
